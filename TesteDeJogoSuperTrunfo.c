77079ca244847a328e254427ab02b67ea1ef0cdb
McLucasMarcos <magalhaeslukas17@gmail.com>
2025-01-23 12:09:07 +0000

    criei um código

---
+++ TesteDeJogoSuperTrunfo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+int nome;
+char codigo;
+float população;
+double área;
+double PIB;
+int turísticos;
+
+printf("Digite o nome da cidade: \n");
+scanf("%d", &nome);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
