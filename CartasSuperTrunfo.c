ba4dc90b18bc4f2ad8536a681ffb8df7ed42c3f3
McLucasMarcos <magalhaeslukas17@gmail.com>
2025-01-23 12:57:20 +0000

    teste de jogo trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,15 +3,31 @@
 int main(){
 
 int nome;
-char codigo;
 float populacao;
-double area;
-double PIB;
+float area;
+float PIB;
 int turisticos;
+char codigo[10];
 
 printf("Digite o nome da cidade: \n");
 scanf("%d", &nome);
 
+printf("Digite a Populacao: \n");
+scanf("%f", &populacao);
+
+printf("Digite a area da cidade: \n");
+scanf("%f", &area);
+
+printf("Digite o pib da cidade: \n");
+scanf("%f", &PIB);
+
+printf("Digite o numero de pontos turisticos: \n");
+scanf("%d", &turisticos);
+
+printf("Digite o Codigo da Cidade: \n");
+scanf("%c", &codigo);
+
+return 0;
 
 
 }
