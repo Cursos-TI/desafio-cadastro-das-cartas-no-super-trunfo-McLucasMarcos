becb526629e177efaed560203e246fb7de457148
McLucasMarcos <magalhaeslukas17@gmail.com>
2025-01-29 18:00:14 +0000

    teste de jogo trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,7 @@ float populacao;
 float area;
 float pib;
 int pontosturisticos;
-char codigo[20];
+char codigo[10];
 char nome[20];
 
 printf("insira a populacao: \n");
@@ -28,13 +28,88 @@ printf("insira o nome da cidade: \n");
 scanf("%s", &nome);
 
 printf("Nome da Cidade: %s\n", nome);
-printf("Código da Cidade %s\n", codigo);
-printf("População %f\n", populacao);
-printf("área %f\n", area);
+printf("Código da Cidade: %s\n", codigo);
+printf("População: %f\n", populacao);
+printf("área: %f\n", area);
 printf("PIB: %f\n", pib);
 printf("Pontos Turísticos: %d\n", pontosturisticos);
 
-return 0;
+printf("insira a populacao: \n");
+scanf("%f", &populacao);
 
+printf("insira a area: \n");
+scanf("%f", &area);
+
+printf("insira o pib: \n");
+scanf("%f", &pib);
+
+printf("insira os pontos turisticos: \n");
+scanf("%d", &pontosturisticos);
+
+printf("insira o código da cidade: \n");
+scanf("%s", &codigo);
+
+printf("insira o nome da cidade: \n");
+scanf("%s", &nome);
+
+printf("Nome da Cidade: %s\n", nome);
+printf("Código da Cidade: %s\n", codigo);
+printf("População: %f\n", populacao);
+printf("área: %f\n", area);
+printf("PIB: %f\n", pib);
+printf("Pontos Turísticos: %d\n", pontosturisticos);
+
+printf("insira a populacao: \n");
+scanf("%f", &populacao);
+
+printf("insira a area: \n");
+scanf("%f", &area);
+
+printf("insira o pib: \n");
+scanf("%f", &pib);
+
+printf("insira os pontos turisticos: \n");
+scanf("%d", &pontosturisticos);
+
+printf("insira o código da cidade: \n");
+scanf("%s", &codigo);
+
+printf("insira o nome da cidade: \n");
+scanf("%s", &nome);
+
+printf("Nome da Cidade: %s\n", nome);
+printf("Código da Cidade: %s\n", codigo);
+printf("População: %f\n", populacao);
+printf("área: %f\n", area);
+printf("PIB: %f\n", pib);
+printf("Pontos Turísticos: %d\n", pontosturisticos);
+
+printf("insira a populacao: \n");
+scanf("%f", &populacao);
+
+printf("insira a area: \n");
+scanf("%f", &area);
+
+printf("insira o pib: \n");
+scanf("%f", &pib);
+
+printf("insira os pontos turisticos: \n");
+scanf("%d", &pontosturisticos);
+
+printf("insira o código da cidade: \n");
+scanf("%s", &codigo);
+
+printf("insira o nome da cidade: \n");
+scanf("%s", &nome);
+
+printf("Nome da Cidade: %s\n", nome);
+printf("Código da Cidade: %s\n", codigo);
+printf("População: %f\n", populacao);
+printf("área: %f\n", area);
+printf("PIB: %f\n", pib);
+printf("Pontos Turísticos: %d\n", pontosturisticos);
+
+
+return 0;
 
 }
